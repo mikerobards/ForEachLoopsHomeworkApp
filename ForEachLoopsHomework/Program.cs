d1c57fec77059ce26eeede53b90ed4fc98895115
Michael Robards <mikerobards@icloud.com>
2022-01-11 15:09:23 -0500

    met all requirements

--- ForEachLoopsHomework/Program.cs
+++ ForEachLoopsHomework/Program.cs
@@ -1,4 +1,25 @@
 ﻿
+string input;
+List<string> firstNames = new();
+
+do
+{
+    Console.Write("Enter the first name (enter blank when finished): ");
+    input = Console.ReadLine();
+    if (input != "")
+    {
+        firstNames.Add(input);
+    }
+} while (input != "");
+
+
+foreach (string name in firstNames)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Hello {name}!");
+}
+
+
 
 
 
